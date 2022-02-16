56cbf03a1fad2b39491f33fa75902a323ecc651a
ganghyoc <ganghyoc@gmail.com>
2022-02-16 23:04:34 +0900

    수정

--- routes/posts.js
+++ routes/posts.js
@@ -11,26 +11,36 @@ router.get("/post", async (req, res) => {
 });
 
 //게시물 생성
+//제목, 작성자, 작성 내용 받기
 router.post("/post", async (req, res) => {
-    const { postId, title, author, content, dateTime } = req.body; 
+    const { title, author, content} = req.body; 
+    dateTime = new Date();
+    const createPost = new Post({ title, author, content, dateTime});
+    await createPost.save();
+    res.send({ success: true });
+});
 
-    const post = await Post.find({ postId }); 
+//게시물 수정
+//제목, 작성자명, 작성 내용 중 원하는 내용 수정하기
+router.put("/post/:postId", async (req, res) => {
+    const { postId } = req.params;
+    const { title, author, content } = req.body; 
+
+    const post = await Post.find({ _id:postId }); 
     if (post.length) {
-        return res.status(400).json({ success: false, errorMessage: "이미있는 게시물 입니다." });
+        await post.updateOne({ postId },{ $set: { title, author, content} });
     }
 
-    const createPost = await Post.create({ postId, title, author, content, dateTime });
-
-    res.json({ post: createPost });
+    res.send({ success: true });
 });
 
 //게시물 상세 조회
+// 제목, 작성자명, 작성 날짜, 작성 내용 조회
 router.get("/post/:postId", async (req, res) => {
     const { postId } = req.params;
-    const [post] = await Post.find({ postId: Number(postId) });
-    res.json({
-        post,
-    });
+    const [post] = await Post.find({ _Id: postId });
+    
+    res.json({ post });
 });
 
 // 개시물 삭제
@@ -38,60 +48,63 @@ router.get("/post/:postId", async (req, res) => {
 router.delete("/post/:postId", async (req, res) => {
     const { postId } = req.params;
 
-    const existspost = await Post.find({ postId: Number(postId) });
+    const existspost = await Post.find({ _Id: postId });
     if (existspost.length) {
-        await Post.deleteOne({ postId: Number(postId) });
-        await Coments.deleteOne({ postId: Number(postId) });
+        await Post.deleteOne({ _id: postId });
+        await Coments.delete({ postId: postId });
     }
 
-    res.json({ success: true });
+    res.send({ success: true });
 });
 
 //댓글
-//개시글에 맞는 댓글  조회
-router.get("/goods/:postId/coments", async (req, res) => {
+//개시글에 맞는 모든 댓글  조회 x
+//날짜 기준 내림차순 정렬 x
+//
+router.get("/post/:postId/coments", async (req, res) => {
     const postId = req.params;
-    const coments = await Coments.find();
-    const goodsIds = carts.map((cart) => cart.goodsId); //
-
-    const goods = await Goods.find({ goodsId: goodsIds });
-    
-    res.json({
-        cart: carts.map((cart) => ({
-            quantity: cart.quantity,
-            goods: goods.find((item) => item.goodsId === cart.goodsId),
-        })),
+    const coments = await Coments.find({ postId: postId }).sort({"dateTime" : -1});
+    res.json({ 
+        coments,
     });
 });
 
+router.post("/post/:postId/coments", async (req, res) => {
+    const postId = req.params;
+    const coments = await Coments.find({ postId: postId }).sort({"dateTime" : -1});
+
+    
+    res.send({ success: true });
+});
+
 //댓글 수정
-//
-router.put("/goods/:postId/cart", async (req, res) => {
-    const { postId } = req.params;
-    const { quantity } = req.body;
+//- 댓글 내용을 비워둔 채 댓글 수정 API를 호출하면 "댓글 내용을 입력해주세요" 라는 메세지를 return하기 o
+//- 댓글 내용을 입력하고 댓글 수정 API를 호출한 경우 작성한 댓글을 수정하기 o
+// router.put("/post/:postId/coments/:comentId", async (req, res) => {
+//     const { postId, comentId } = req.params;
+//     const { postId, author, content } = req.body;
 
-    const existspost = await Carts.find({ postId: Number(postId) });
-    if (!existscarts.length) {
-        await Coments.create({ goodsId: Number(postId), quantity});
-    } else {
-        await Coments.updateOne({ goodsId: Number(postId) },{ $set: { quantity} });
+//     const existscoments = await Coments.find({ postId: postId, _id: comentId });
+//     if (!existscoments.length) {
+//         return res.status(404).json({ success: false, errorMessage: "댓글 내용을 입력해주세요" });
+//     } else {
+//         await Coments.updateOne({ postId: postId) },{ $set: { author, content} });
+//     }
+
+//     res.send({ success: true });
+// });
+
+//댓글 삭제
+router.delete("/coments/:comentId", async (req, res) => {
+    const { comentId } = req.params;
+
+    const existscoments = await Post.find({ comentId: comentId });
+    if (existscoments.length) {
+        await Coments.delete({ comentId });
     }
-
 
     res.json({ success: true });
 });
-
-//댓글 삭제
-router.delete("/goods/:goodsId/cart", async (req, res) => {
-    const { goodsId } = req.params;
-
-    const existscart = await Carts.find({ goodsId: Number(goodsId) });
-    if (existscart.length) {
-        await Cart.deleteOne({ goodsId: Number(goodsId) });
-    }
-
-    res.json({ success: true });
-})
 
 
 module.exports = router;
