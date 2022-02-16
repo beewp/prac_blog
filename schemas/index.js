bf4a7f261bd0f6a1f2d08b8edfa69eac7e2390c3
ganghyoc <ganghyoc@gmail.com>
2022-02-17 07:31:03 +0900

    dev 옵션 추가

--- schemas/index.js
+++ schemas/index.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
+
 const connect = () => {
+    mongoose.set('debug', true);
     mongoose
         .connect("mongodb://localhost:27017/spa_blog")
         .catch(err => console.log(err));
@@ -9,5 +11,7 @@ const connect = () => {
 mongoose.connection.on("error", err => {
     console.error("몽고디비 연결 에러", err);
 });
+
+
 
 module.exports = connect;
